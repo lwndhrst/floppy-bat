d50ca4f1f550785aeed9be2b4f4fa8d4f7095355
Leon Windhorst <leon@windhorst.dev>
2024-04-30 16:32:07 +0200

    chore: removed 'SetTargetFPS'

--- src/main.c
+++ src/main.c
@@ -6,15 +6,14 @@
 int
 main(void)
 {
-    const int screen_width = 600;
-    const int screen_height = 400;
-
     // ***********************************************
     // init
     // ***********************************************
 
+    const int screen_width = 600;
+    const int screen_height = 400;
+
     InitWindow(screen_width, screen_height, "Floppy Bat");
-    SetTargetFPS(60);
 
     Player player;
     init_player(&player, screen_width, screen_height);
